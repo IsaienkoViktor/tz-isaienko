88f5d406071fc35fe330e1781e9d5ef931ed5374
IsaienkoViktor <isaenkov1986@gmail.com>
2023-07-31 18:56:50 +0300

    FIXED_push problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,13 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getUserThunk } from "./redux/userThunk";
 import { Card } from "./components/Card/UserCard";
-import { Container } from "./components/Container/Container";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUserThunk());
   }, [dispatch]);
-  return (
-    <>
-      <Container>
-        <Card />
-      </Container>
-    </>
-  );
+  return <Card />;
 }
 
 export default App;
--- src/components/Container/Wrapper.styled.js
+++
@@ -1,8 +0,0 @@
-import { styled } from "styled-components";
-
-export const CardWrapper = styled.div`
-  justify-content: center;
-  display: flex;
-  align-items: center;
-  width: 1440px;
-`;
